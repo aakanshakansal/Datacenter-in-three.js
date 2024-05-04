ed70701cff37f93a8595a55eef8146f27bea7f3d
aakanshakansal <aakanshakansal56@gmail.com>
2024-05-04 18:07:25 +0530

    second commit

--- apiKey.js
+++ apiKey.js
@@ -1,11 +1,4 @@
-const apiKey = "paste your apiKey from croquet.io/keys";
-const appId = "type your own appId such as com.example.david.mymicroverse";
-export default {apiKey, appId};
-
-// For development on local computer and local network, use a development key
-// from croquet.io/keys and create a file called apiKey-dev.js.
-
-// For deployment to a publicly accessible server, create a production key
-// from croquet.io/keys and create a file called apiKey.js.
-
-// you may export other Croquet session parameters to override default values.
+/* Copy this into a file named apiKey.js */
+const apiKey = "1ey3BKzAEHvs6oavmaqIexksdJoujeaPqus3xgd0s";
+const appId = "com.gmail.aayushkansal16.microverse";
+export default { apiKey, appId };
